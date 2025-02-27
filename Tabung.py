c625fdcc51f181c858c4d096d2ca317296d2b8e8
Ama-nir <SYAHRIALYT2@gmail.com>
2025-02-27 10:43:44 +0700

    Add files via upload

---
+++ Tabung.py
@@ -0,0 +1,8 @@
+#Volume Tabung
+
+print("Volume Tabung")
+radius = float(input("Masukkan radian dari tabung: "))
+tinggi = float(input("Masukkan tinggi dari tabung: "))
+volume_tabung = 3.14 * radius ** 2 * tinggi
+
+print("Luas dari tabung adalah:", volume_tabung)
