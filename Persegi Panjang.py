0cc2cb3795439521ab4449a62c8dd8b05d1a2e52
Ama-nir <SYAHRIALYT2@gmail.com>
2025-02-27 10:43:33 +0700

    Add files via upload

---
+++ Persegi Panjang.py
@@ -0,0 +1,7 @@
+#Luas Persegi Panjang
+
+print("Luas Persegi Panjang")
+lebar = float(input("Masukkan lebar dari persegi panjang: "))
+panjang = float(input("Masukkan panjang dari persegi panjang: "))
+
+print("Luas dari persegi panjang adalah:", lebar * panjang)
