2f9d1db049c7eb7ba016d7e2568f486a92339b8a
Ama-nir <SYAHRIALYT2@gmail.com>
2025-02-27 10:42:58 +0700

    Add files via upload

---
+++ Luas Lingkaran.py
@@ -0,0 +1,7 @@
+#Luas Lingkaran
+
+print("Luas Lingkaran")
+radius = float(input("Masukkan radian dari lingkaran: "))
+luas_lingkaran = 3.14 * radius ** 2
+
+print("Luas lingkarannya adalah:", luas_lingkaran)
